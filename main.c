86ffc335a171dc3e7f2bf8f55149e858b66fb7d3
RockThunderheart <alexdaigre@gmail.com>
2018-09-26 20:18:24 -0500

    passing stuff

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/ipc.h>
+#include  <sys/types.h>
+#include <sys/shm.h>
 
 void abortExecution(int status);
 
 int main (int argc, char *argv[]) {
     int numberOfChildren = 0;
-    int maxProcesses = INT32_MAX;
+    int maxProcesses = __INT32_MAX__;
     int currentProcesses = 0;
     int c;
 
@@ -30,9 +33,28 @@ int main (int argc, char *argv[]) {
         }
     }
 
+    int clockShmId = shmget(IPC_PRIVATE, 2*sizeof(int), IPC_CREAT | 0666);
+    if (clockShmId < 0) {
+        printf("shmget error in parrent\n");
+        abortExecution(1);
+    }
+
+    int* clockShmPtr = (int *) shmat(clockShmId, NULL, 0);
+    if ((int) clockShmPtr == -1) {
+        printf("shmat error in parrent\n");
+        abortExecution(1);
+    }
+
     for(int i = 0; i < numberOfChildren; i++){
         if (fork() == 0){
-            execlp("./worker","./worker",numberOfChildren,NULL);
+            char numberOfChildrenString[12];
+            sprintf(numberOfChildrenString, "%d", numberOfChildren);
+            char alocatedIntOneString[12];
+            sprintf(alocatedIntOneString, "%d", clockShmPtr[0]);
+            char alocatedIntTwoString[12];
+            sprintf(alocatedIntTwoString, "%d", clockShmPtr[1]);
+
+            execlp("./worker","./worker", numberOfChildrenString, alocatedIntOneString, alocatedIntTwoString, NULL);
 		    fprintf(stderr,"%s failed to exec worker!\n",argv[0]);
 		    exit(-1);
             abortExecution(0);
--- worker.c
+++ worker.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/shm.h>
 
 void abortExecution(int status);
 
 int main (int argc, char *argv[]) {
     int numberOfloops = atoi(argv[1]);
-
+    printf("number of loops: %d\n", numberOfloops);
+    int sharedMemoryIntOne = atoi(argv[2]);
+    printf("SharedMem1: %d\n", sharedMemoryIntOne);
+    int sharedMemoryIntTwo = atoi(argv[3]);
+    printf("SharedMem2: %d\n", sharedMemoryIntTwo);
+    
     for(int i = 0; i < numberOfloops; i++){
-        printf("Hi");
+        printf("Hi %d %d\n", sharedMemoryIntOne, sharedMemoryIntTwo);
     }
     
     abortExecution(0);
